b0fda3a5219cf7bc8c46b1773897f88b4a03469a
Ray Nham <ray@raydeveloperonline.com>
2018-03-08 22:15:36 +1100

    Initial commit

---
+++ Java/code1.java
@@ -0,0 +1,60 @@
+// Create the strategy class.
+public interface FlyStrategy{
+    public void fly();
+}
+
+// The first concrete strategy class.
+public class FlyStrategy1 implements Flyable{
+    @Override
+    public void fly(){
+        System.out.println("I can fly.");
+    }
+}
+
+// The second concrete strategy class.
+public class FlyStrategy2 implements Flyable{
+    @Override
+    public void fly(){
+        System.out.println("I cannot fly.");
+    }
+}
+
+// Create the Context class.
+public class Vehicle{
+    // This member field specifies which strategy to implement.
+    protected FlyStrategy strategy;
+    // Define the constructor.
+    public Vehicle (FlyStrategy strategy){
+        this.strategy = strategy;
+    }
+    // Define a method to print which strategy the object has.
+    public void tryToFly(){
+        strategy.fly();
+    }
+    // Define a method to change the object's strategy.
+    public boolean setFlyStrategy(FlyStrategy strategy){
+        this.strategy = strategy;
+    }
+}
+
+// Define the child classes.
+public class Plane extends Vehicle{
+    public Plane(){
+        super(new FlyStrategy1());
+    }
+}
+public class Car extends Vehicle{
+    public Car(){
+        super(new FlyStrategy2());
+    }
+}
+
+// If we have another child class that has the same behaviour with 
+// either the two classes above, we will not need to duplicate the code 
+// again.
+// aka. Resusability has been improved.
+public class Aircraft extends Vehicle{
+    public Aircraft(){
+        super(new FlyStrategy1());
+    }
+}
---
+++ Java/traditional-implementation.java
@@ -0,0 +1,29 @@
+/* 
+	File: /Strategy-Design-Pattern/Java/traditional-implementation.java
+	Description: This example demonstrates a simple example of strategy pattern design.
+	Author: Ray Nham (ray@raydeveloperonline.com)
+	Date: 25/02/2018
+	Blog post: https://raydeveloperonline.com/2018/02/25/strategy-design-pattern/
+ */
+public interface Flyable{
+	// Required method of this interface.
+	public void fly();
+}
+
+public abstract class Vehicle implements Flyable{
+	// Member variables and abstract method go here.
+}
+
+public class Car extends Vehicle{
+	// Concrete implementation for Flyable interface.
+	public void fly(){
+		System.out.println("I cannot fly.");
+	}
+}
+
+public class Car extends Plane{
+	// Concrete implementation for Flyable interface.
+	public void fly(){
+		System.out.println("I can fly.");
+	}
+}
